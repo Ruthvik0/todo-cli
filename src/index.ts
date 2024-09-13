53422ce33fddf23efa072b45927110d51e9cf368
Ruthvik <92439756+Ruthvik0@users.noreply.github.com>
2024-09-13 09:28:41 +0000

    Added types to read todo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
-import { Command } from 'commander';
-const program = new Command();
+import { input } from "@inquirer/prompts";
+import { Command } from "commander";
+
+type Todo = {
+  task: String;
+  createdAt: Date;
+  completedAt: Date | null;
+  category: String;
+};
+
+type Category = {
+  name: String;
+};
+
+const todoList: Todo[] = [];
+const categoryList: Category[] = [];
+
+const program = new Command("Todo");
+
+program
+  .name("Todo-cli")
+  .description("Manage your Todo's from CLI")
+  .action(() => {
+    console.log("Hello");
+  });
+
+program
+  .command("add")
+  .description("Add new todo")
+  .action(async () => {
+    const answer = await input({ message: "What are you planning?" });
+    console.log(answer);
+  });
 
 program.parse(process.argv);
